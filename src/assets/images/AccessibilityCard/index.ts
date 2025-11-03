ba690b370637c3aa28dc177323a741d3fe9762b0
aek676 <aek676@inlumine.ual.es>
2025-11-03 18:41:00 +0100

    feat: agregar exportaciones de imágenes para AccessibilityCard y simplificar importaciones en LandingPage

---
+++ src/assets/images/AccessibilityCard/index.ts
@@ -0,0 +1,7 @@
+// Exportaciones de imágenes para AccessibilityCard
+export { default as ConsultoriaAccesibilidadWeb } from './Consultoria-Accesibilidad-Web.svg';
+export { default as LogoWeAllWeb } from './Logo-we-all-web.svg';
+export { default as Auditoria } from './Auditoria.svg';
+export { default as DisenoWeb } from './Diseno-web.svg';
+export { default as AccesibilidadAplicacionesMoviles } from './Accesibilidad-Aplicaciones-Moviles.svg';
+export { default as DocumentosPDFAccesibles } from './Documentos-y-PDFs-Accesibles.svg';
---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+// Importaciones principales
+export { default as WeAllWebLogotipoBlack } from './We-all-web-logotipo-black.webp';
+export { default as IconosAccesibilidad } from './iconos-de-accesibilidad-350x77.webp';
+export { default as HerramientaDeValidacionAccesibilidad } from './Herramienta-de-validacion-accesibilidad-web.webp';
+export { default as QuienEstaObligado } from './Quien-Esta-Obligado.webp';
+
+export * from './AccessibilityCard/index';
