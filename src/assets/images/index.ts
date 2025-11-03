bbbc447bd952c06367f4c7fd2341d8871ebe6fa7
aek676 <aek676@inlumine.ual.es>
2025-11-03 23:22:18 +0100

    feat: agregar nuevos íconos y componente DarkButton, además de mejorar estilos en LandingPage

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,8 +1,11 @@
-// Importaciones principales
 export { default as WeAllWebLogotipoBlack } from './We-all-web-logotipo-black.webp';
 export { default as IconosAccesibilidad } from './iconos-de-accesibilidad-350x77.webp';
 export { default as HerramientaDeValidacionAccesibilidad } from './Herramienta-de-validacion-accesibilidad-web.webp';
 export { default as QuienEstaObligado } from './Quien-Esta-Obligado.webp';
+export { default as IconoEficiencia } from './icono-eficiencia.png';
+export { default as IconoLegislacion } from './icono-legislacion.png';
+export { default as IconoMercado } from './icono-mercado.webp';
+export { default as IconoResponsabilidad } from './icono-responsabilidad.webp';
 
 export * from './AccessibilityCard/index';
 export * from './Carousel/index';
